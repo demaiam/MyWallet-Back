fa0bf8e18fecbd87a3db1636dcba13a302acdeb9
andre <demaiam@gmail.com>
2023-07-11 11:57:50 -0300

    returned params

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -8,10 +8,10 @@ export async function signup(req, res) {
   const hash = bcrypt.hashSync(password, 10);
 
   try {
-    const searchUser = await db.collection("users").findOne({ email });
-    if (searchUser) return res.status(409).send("Email já cadastrado");
+    const user = await db.collection("users").findOne({ email });
+    if (user) return res.status(409).send("Email já cadastrado");
 
-    await db.collection("users").insertOne({ name, email, password: hash });
+    await db.collection("users").insertOne({ name: name, email: email, password: hash });
     res.sendStatus(201);
   } catch (err) {
     res.status(500).send(err.message);
@@ -22,18 +22,18 @@ export async function signin(req, res) {
   const { email, password } = req.body;
 
   try {
-    const searchUser = await db.collection("users").findOne({ email });
-    if (!searchUser) return res.status(404).send("Email não cadastrado");
+    const user = await db.collection("users").findOne({ email });
+    if (!user) return res.status(404).send("Email não cadastrado");
 
-    const isPasswordCorrect = bcrypt.compareSync(password, searchUser.password);
+    const isPasswordCorrect = bcrypt.compareSync(password, user.password);
     if (!isPasswordCorrect) return res.status(401).send("Senha incorreta");
 
     const token = uuid();
 
-    await db.collection("session").deleteMany({ idUser: searchUser._id });
-    await db.collection("session").insertOne({ token, idUser: searchUser._id });
+    await db.collection("session").deleteMany({ idUser: user._id });
+    await db.collection("session").insertOne({ token, idUser: user._id });
 
-    const data = { name: searchUser.name, token: token };
+    const data = { name: user.name, token: token };
 
     res.status(200).send(data);
   } catch (err) {
